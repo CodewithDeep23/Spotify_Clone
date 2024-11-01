234337a5f0c0b4d135f73c8c4bc7bff667452c61
CodewithDeep23 <deepankarsinghvm@gmail.com>
2024-11-01 17:12:56 +0530

    edit in search bar

--- script.js
+++ script.js
@@ -3,17 +3,18 @@
 //     box1.style.backgroundColor = "blue";
 // })
 
-document.querySelector(".search_input_part").addEventListener("click", () => {
-  document.querySelector(".search_input_part").style.border = "1px solid white";
+document.querySelector(".search-bar").addEventListener("click", () => {
+  document.querySelector(".search-bar").style.border = "1px solid white";
 })
-document.querySelector(".search_input_part").addEventListener("mouseenter", () => {
-  document.querySelector(".search_input_part").style.backgroundColor = "#2A2A2A";
+document.querySelector(".search-bar").addEventListener("mouseenter", () => {
+  document.querySelector(".search-bar").style.backgroundColor = "#2A2A2A";
   document.querySelector("#inp").style.backgroundColor = "#2A2A2A";
   document.getElementById("searchIcon").src = "Images/search-hover.svg"
 })
-document.querySelector(".search_input_part").addEventListener("mouseleave", () => {
-  document.querySelector(".search_input_part").style.border = "none";
-  document.querySelector(".search_input_part").style.backgroundColor = "var(--leftScroll)";
+document.querySelector(".search-bar").addEventListener("mouseleave", () => {
+  document.querySelector(".search-bar").style.border = "none";
+  document.querySelector(".search-bar").style.backgroundColor = "var(--leftScroll)";
+  // document.querySelector(".search-bar").style.overflow = "hidden";
   document.querySelector("#inp").style.backgroundColor = "var(--leftScroll)";
   document.getElementById("searchIcon").src = "Images/search.svg"
 })
